506506525b613fe05e398c93ed2d659a7428b1fb
Gustav Fredrikson <gustavfredrikson98@gmail.com>
2023-05-23 13:17:05 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
-sudo cp ~/DE2-GithubAnalytics/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+#!/bin/bash
+
+# copy the configuration file to the appropriate directory
+sudo cp /home/ubuntu/DE2-GithubAnalytics/supervisord.conf /etc/supervisor/supervisord.conf
+
+# reload supervisor with the new configuration
 sudo supervisorctl reread
+
+# update and start services
 sudo supervisorctl update
 sudo supervisorctl start all
-sudo supervisorctl status
