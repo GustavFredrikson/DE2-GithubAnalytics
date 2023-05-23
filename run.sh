8b20210b3fcb44d26443fd39b5c3298622541e48
Gustav Fredrikson <gustavfredrikson98@gmail.com>
2023-05-23 17:31:05 +0200

    run

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Step 1: Create logs directory if it doesn't exist
-mkdir -p ~/DE2-GithubAnalytics/logs/
+mkdir -p /home/ubuntu/DE2-GithubAnalytics/logs/
 
 # Step 1.5 Remove previous logs
-sudo rm ~/DE2-GithubAnalytics/logs/*.log
+sudo rm /home/ubuntu/DE2-GithubAnalytics/logs/*.log
 
 # Step 2: Install Python packages
 pip3 freeze | xargs pip uninstall -y
 
-python3 -m pip install -r ~/DE2-GithubAnalytics/requirements.txt
+python3 -m pip install -r /home/ubuntu/DE2-GithubAnalytics/requirements.txt
 
 # Step 3: Copy the supervisord.conf to /etc/supervisor/conf.d/
 sudo cp /home/ubuntu/DE2-GithubAnalytics/supervisord.conf /etc/supervisor/supervisord.conf
