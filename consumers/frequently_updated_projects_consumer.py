4da5e455940e1dd9c4f66fc8908f14652249bf6b
Gustav Fredrikson <gustavfredrikson98@gmail.com>
2023-05-24 10:47:27 +0200

    int

--- consumers/frequently_updated_projects_consumer.py
+++ consumers/frequently_updated_projects_consumer.py
@@ -12,19 +12,28 @@ message_count = 0
 save_interval = 1000
 
 while True:
-    msg = consumer.receive()
-    repo = json.loads(msg.data())
+    try:
+        msg = consumer.receive()
+        repo = json.loads(msg.data())
 
-    if repo["name"] in df.index:
-        df.loc[repo["name"], "commits"] += int(repo["commits"])
-    else:
-        df.at[repo["name"], "commits"] = int(repo["commits"])
+        if repo["name"] in df.index:
+            df.loc[repo["name"], "commits"] += int(
+                repo["commits"]
+            )  # Ensure it's integer
+        else:
+            df.at[repo["name"], "commits"] = int(repo["commits"])  # Ensure it's integer
 
-    most_commits = df.nlargest(10, "commits")
-    print("Top 10 frequently updated projects: ", most_commits)
-    # Save to file every save_interval messages
-    if message_count % save_interval == 0:
-        most_commits.to_csv("most_commits.csv")
-    consumer.acknowledge(msg)
+        message_count += 1
+
+        most_commits = df.nlargest(10, "commits")
+        print("Top 10 frequently updated projects: ", most_commits)
+
+        # Save to file every save_interval messages
+        if message_count % save_interval == 0:
+            most_commits.to_csv("most_commits.csv")
+
+        consumer.acknowledge(msg)
+    except Exception as e:
+        print("Error: ", e)
 
 client.close()
