4749a998fd9954c4f9df37944ec85b28637d0f71
Gustav Fredrikson <gustavfredrikson98@gmail.com>
2023-05-24 09:51:52 +0200

    pulsar

--- consumers/programming_languages_consumer.py
+++ consumers/programming_languages_consumer.py
@@ -1,43 +1,33 @@
-from pulsar import ConsumerType
 import pulsar
 import json
-from collections import defaultdict
+import pandas as pd
 
-# Pulsar client
 client = pulsar.Client("pulsar://localhost:6650")
 
-# Pulsar consumer
-consumer = client.subscribe(
-    "LanguageTopic", "language_consumer_sub", consumer_type=ConsumerType.Shared
-)
+consumer = client.subscribe("ProgrammingLanguagesTopic", "my-subscription")
 
-# Maintain a count of the number of repositories for each language
-language_counts = defaultdict(int)
+# DataFrame to keep track of language counts
+df = pd.DataFrame(columns=["language", "count"])
+df.set_index("language", inplace=True)
 
-try:
-    while True:
-        # Receive messages from the LanguageTopic
+while True:
+    try:
         msg = consumer.receive()
+        repo = json.loads(msg.data())
 
-        try:
-            # Decode the message
-            language_data = json.loads(msg.data())
+        # Update the DataFrame with the language of the new repository
+        if repo["language"] in df.index:
+            df.loc[repo["language"], "count"] += 1
+        else:
+            df.loc[repo["language"]] = {"count": 1}
 
-            # Update the language counts
-            if language_data["language"] is not None:
-                language_counts[language_data["language"]] += 1
+        # Calculate and print the top 10 languages
+        top_languages = df.nlargest(10, "count")
+        print("Top 10 languages: ", top_languages)
 
-            # Acknowledge successful processing of the message
-            consumer.acknowledge(msg)
+        # Acknowledge processing of message so that it can be deleted
+        consumer.acknowledge(msg)
+    except Exception as e:
+        print("Error: ", e)
 
-        except:
-            # Message failed to process
-            consumer.negative_acknowledge(msg)
-
-finally:
-    # Close the client
-    client.close()
-
-# Print the counts
-for language, count in language_counts.items():
-    print(f"{language}: {count}")
+client.close()
--- producers/programming_languages_producer.py
+++ producers/programming_languages_producer.py
@@ -1,37 +1,27 @@
-from pulsar import ConsumerType
 import pulsar
 import json
-from decouple import config
 
-# Pulsar client
 client = pulsar.Client("pulsar://localhost:6650")
 
-# Pulsar consumers and producers
-consumer = client.subscribe(
-    "MainGithubRepoTopic", "language_producer_sub", consumer_type=ConsumerType.Shared
-)
-producer = client.create_producer("LanguageTopic")
+consumer = client.subscribe("MainGithubRepoTopic", "my-subscription")
+producer = client.create_producer("ProgrammingLanguagesTopic")
 
-try:
-    while True:
-        # Receive messages from the MainGitHubRepoTopic
+while True:
+    try:
         msg = consumer.receive()
+        repo = json.loads(msg.data())
+        print(f"Received message: '{repo}' id='{msg.message_id()}'")
 
-        try:
-            # Decode the message
-            repo = json.loads(msg.data())
+        # Forward data to the ProgrammingLanguagesTopic
+        producer.send(
+            json.dumps({"name": repo["name"], "language": repo["language"]}).encode(
+                "utf-8"
+            )
+        )
 
-            # Send only the language data to the LanguageTopic
-            language_data = {"id": repo["id"], "language": repo["language"]}
-            producer.send(json.dumps(language_data).encode("utf-8"))
+        # Acknowledge processing of message so that it can be deleted
+        consumer.acknowledge(msg)
+    except Exception as e:
+        print("Error: ", e)
 
-            # Acknowledge successful processing of the message
-            consumer.acknowledge(msg)
-
-        except:
-            # Message failed to process
-            consumer.negative_acknowledge(msg)
-
-finally:
-    # Close the client
-    client.close()
+client.close()
