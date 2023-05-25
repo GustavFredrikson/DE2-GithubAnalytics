9fe93514cee621eafc1a6c98b605b81bc99b4685
Gustav Fredrikson <gustavfredrikson98@gmail.com>
2023-05-25 10:06:08 +0200

    commits per day

--- producers/frequently_updated_projects_producer.py
+++ producers/frequently_updated_projects_producer.py
@@ -2,6 +2,7 @@ import pulsar
 import requests
 import json
 from decouple import config
+from datetime import datetime
 
 TOKEN = config("GITHUB_API_TOKEN")
 HEADERS = {
@@ -25,16 +26,47 @@ while True:
 
     try:
         commits = 0
+        first_commit_date = None
+        last_commit_date = None
         while True:
             response = requests.get(url, headers=HEADERS, params=params)
             response.raise_for_status()
-            commits += len(response.json())
+
+            data = response.json()
+            commits += len(data)
+            if len(data) > 0:
+                current_first_commit_date = datetime.strptime(
+                    data[-1]["commit"]["committer"]["date"], "%Y-%m-%dT%H:%M:%SZ"
+                )
+                current_last_commit_date = datetime.strptime(
+                    data[0]["commit"]["committer"]["date"], "%Y-%m-%dT%H:%M:%SZ"
+                )
+
+                if (
+                    first_commit_date is None
+                    or current_first_commit_date < first_commit_date
+                ):
+                    first_commit_date = current_first_commit_date
+                if (
+                    last_commit_date is None
+                    or current_last_commit_date > last_commit_date
+                ):
+                    last_commit_date = current_last_commit_date
+
             if "next" not in response.links:
                 break
             params["page"] += 1
 
         repo_with_commits = repo
         repo_with_commits["commits"] = commits
+        if first_commit_date and last_commit_date:
+            days_difference = (
+                last_commit_date - first_commit_date
+            ).days or 1  # avoid division by zero
+            repo_with_commits["commit_frequency"] = (
+                commits / days_difference
+            )  # average commits per day
+
         producer.send(json.dumps(repo_with_commits).encode("utf-8"))
         consumer.acknowledge(msg)
 
