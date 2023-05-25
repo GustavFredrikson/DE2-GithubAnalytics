769940e78ee3af410aa17f8d6fda818046406fe2
Gustav Fredrikson <gustavfredrikson98@gmail.com>
2023-05-25 10:08:16 +0200

    frequency

--- consumers/frequently_updated_projects_consumer.py
+++ consumers/frequently_updated_projects_consumer.py
@@ -5,10 +5,11 @@ import pandas as pd
 client = pulsar.Client("pulsar://localhost:6650")
 consumer = client.subscribe("CommitsTopic", "my-subscription")
 
-df = pd.DataFrame(columns=["name", "commits"])
+df = pd.DataFrame(columns=["name", "commits", "commit_frequency"])
 df.set_index("name", inplace=True)
 
 df["commits"] = df["commits"].astype("int")
+df["commit_frequency"] = df["commit_frequency"].astype("float")
 
 message_count = 0
 save_interval = 1000
@@ -16,7 +17,7 @@ save_interval = 1000
 # If file exists, load it
 try:
     df = pd.read_csv("most_commits.csv", index_col="name")
-except:
+except FileNotFoundError:
     pass
 
 while True:
@@ -28,13 +29,20 @@ while True:
             df.loc[repo["name"], "commits"] += int(
                 repo["commits"]
             )  # Ensure it's integer
+            df.loc[repo["name"], "commit_frequency"] = (
+                df.loc[repo["name"], "commit_frequency"] + repo["commit_frequency"]
+            ) / 2  # Average frequency
         else:
             df.at[repo["name"], "commits"] = int(repo["commits"])  # Ensure it's integer
+            df.at[repo["name"], "commit_frequency"] = repo["commit_frequency"]
 
         message_count += 1
 
         most_commits = df.nlargest(10, "commits")
-        print("Top 10 frequently updated projects: ", most_commits)
+        print("Top 10 projects by total commits: ", most_commits)
+
+        most_frequent_commits = df.nlargest(10, "commit_frequency")
+        print("Top 10 projects by commit frequency: ", most_frequent_commits)
 
         # Save all data to file every save_interval messages
         if message_count % save_interval == 0:
