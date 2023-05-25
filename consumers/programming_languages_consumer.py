1a8ed286542a765aa15c7eb9313daaa6e9267f27
Gustav Fredrikson <gustavfredrikson98@gmail.com>
2023-05-25 10:56:48 +0200

    nolang

--- consumers/programming_languages_consumer.py
+++ consumers/programming_languages_consumer.py
@@ -26,14 +26,14 @@ while True:
         msg = consumer.receive()
         repo = json.loads(msg.data())
 
-        # If no language is specified, set it to "No Language"
-        language = repo["language"] if repo["language"] else "No Language"
-
         # Update the DataFrame with the language of the new repository
         if repo["language"] in df.index:
             df.loc[repo["language"], "count"] += 1
         else:
             df.at[repo["language"], "count"] = 1
+
+        # Fill NA values in language column with "No Language"
+        df["language"].fillna("No Language", inplace=True)
 
         message_count += 1
 
