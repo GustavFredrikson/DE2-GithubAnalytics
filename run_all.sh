3b6c568d12d0ca5da892c8cd1bddc7a711bbfa6b
Gustav Fredrikson <gustavfredrikson98@gmail.com>
2023-05-25 18:23:31 +0200

    w8 with devops

--- run_all.sh
+++ run_all.sh
@@ -9,18 +9,18 @@ IMAGE="gustavfredrikson/de2python"
 # Define your pod names and script names
 declare -A PRODUCER_PODS=(
   ["main-repo-producer-pod"]="producers/main_repo_producer.py"
-  ["devops-producer-pod"]="producers/devops_producer.py"
   ["frequently-updated-projects-producer-pod"]="producers/frequently_updated_projects_producer.py"
   ["programming-languages-producer-pod"]="producers/programming_languages_producer.py"
-  ["test-driven-development-producer-pod"]="producers/test_driven_development_producer.py"
+  # ["devops-producer-pod"]="producers/devops_producer.py"
+  # ["test-driven-development-producer-pod"]="producers/test_driven_development_producer.py"
 )
 
 declare -A CONSUMER_PODS=(
   ["main-repo-consumer-pod"]="consumers/main_repo_consumer.py"
-  ["devops-consumer-pod"]="consumers/devops_consumer.py"
   ["frequently-updated-projects-consumer-pod"]="consumers/frequently_updated_projects_consumer.py"
   ["programming-languages-consumer-pod"]="consumers/programming_languages_consumer.py"
-  ["test-driven-development-consumer-pod"]="consumers/test_driven_development_consumer.py"
+  # ["devops-consumer-pod"]="consumers/devops_consumer.py"
+  # ["test-driven-development-consumer-pod"]="consumers/test_driven_development_consumer.py"
 )
 
 # Function to create a pod, copy a script to it, and run the script
