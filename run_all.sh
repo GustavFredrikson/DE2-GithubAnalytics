57e44c6ee2f3f4ee7205b7f4e376ceef54b47bd6
Gustav Fredrikson <gustavfredrikson98@gmail.com>
2023-05-26 14:19:54 +0200

    clean

--- run_all.sh
+++ run_all.sh
@@ -66,10 +66,6 @@ function create_and_run_pod {
     echo "Listing all available pods..."
     microk8s kubectl get pods --namespace=$NAMESPACE
 
-    # Check the status of the pod
-    echo "Checking status for pod $POD_NAME..."
-    microk8s kubectl describe pod $POD_NAME --namespace=$NAMESPACE
-
     # Copy the script to the pod
     echo "Copying script $SCRIPT_NAME to pod $POD_NAME..."
     microk8s kubectl cp $SCRIPT_NAME $NAMESPACE/$POD_NAME:/tmp/$(basename $SCRIPT_NAME)
